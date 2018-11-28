0c31805db4231c2430cc23fbc23c39652d58a657
Maneet08 <abhimanyutokas@gmail.com>
2018-11-28 14:16:19 +0530

    Updates

---
+++ precache-manifest.7ab20a7b43c16fc7a2b9a2eb60c03a3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc9d19d1a6f2f676f3ed",
+    "url": "/Githubrepo/static/js/runtime~main.fc9d19d1.js"
+  },
+  {
+    "revision": "19e793bf9a19a2ea6587",
+    "url": "/Githubrepo/static/js/main.19e793bf.chunk.js"
+  },
+  {
+    "revision": "88c937af0286dc1af7aa",
+    "url": "/Githubrepo/static/js/1.88c937af.chunk.js"
+  },
+  {
+    "revision": "19e793bf9a19a2ea6587",
+    "url": "/Githubrepo/static/css/main.d175d3e7.chunk.css"
+  },
+  {
+    "revision": "88c937af0286dc1af7aa",
+    "url": "/Githubrepo/static/css/1.bd1599c8.chunk.css"
+  },
+  {
+    "revision": "1b9b04b1b6d7d1acb4389d3d0aafdfb2",
+    "url": "/Githubrepo/index.html"
+  }
+];
